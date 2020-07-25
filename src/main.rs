f139f9e9a1ba8c360e1957c26ebd41bbb101dca1
Fabian Thorand <f.thorand@gmail.com>
2020-07-25 21:37:29 +0200

    Run tasks in parallel where possible

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use ansi_term::Color;
 use snafu::{ResultExt, Snafu};
 use std::ffi::OsString;
+use std::path::Path;
+use std::sync::mpsc;
 
 mod autofile;
 mod queue;
-use std::path::PathBuf;
+use std::{collections::HashSet, path::PathBuf};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -30,9 +32,8 @@ fn run(opt: Opt) -> Result<()> {
     let source = std::fs::read_to_string(&opt.autofile).context(LoadConfig {
         path: opt.autofile.clone(),
     })?;
-    let autofile: autofile::AutoFile = toml::from_str(&source).context(ParseConfig {
-        path: opt.autofile,
-    })?;
+    let autofile: autofile::AutoFile =
+        toml::from_str(&source).context(ParseConfig { path: opt.autofile })?;
 
     let mut plan = queue::TaskQueue::new(autofile.tasks.iter().map(|(id, task)| {
         queue::Task {
@@ -58,49 +59,81 @@ fn run(opt: Opt) -> Result<()> {
     let mut successful = Vec::new();
     let mut failed = Vec::new();
 
-    while let Some(task) = plan.pop_available() {
-        eprintln!("{} {}", Color::Blue.bold().paint("Running"), task.id);
+    let (sender, receiver) = mpsc::channel::<TaskMessage>();
 
-        // Create files for redirecting output
-        let task_stdout_path = outdir
-            .path()
-            .join(&task.id.as_str())
-            .with_extension("stdout");
-        let task_stderr_path = outdir
-            .path()
-            .join(&task.id.as_str())
-            .with_extension("stderr");
-        let task_stdout = std::fs::File::create(task_stdout_path).context(Temp)?;
-        let task_stderr = std::fs::File::create(task_stderr_path).context(Temp)?;
+    let mut running = HashSet::new();
 
-        let start_time = std::time::Instant::now();
-        let mut cmd = std::process::Command::new(&task.payload.program)
-            .args(&task.payload.arguments)
-            .stdout(task_stdout)
-            .stderr(task_stderr)
-            .spawn()
-            .context(TaskStart {
-                id: task.id.clone(),
-            })?;
+    loop {
+        while let Some(task) = plan.pop_available() {
+            eprintln!("{} {}", Color::Blue.bold().paint("Running"), task.id);
+            running.insert(task.id.clone());
 
-        let status = cmd.wait().context(TaskWait {
-            id: task.id.clone(),
-        })?;
-        let duration = start_time.elapsed();
+            std::thread::spawn({
+                let sender = sender.clone();
+                let outdir_path = outdir.path().to_path_buf();
+                move || {
+                    let start_time = std::time::Instant::now();
 
-        let msg = if status.success() {
-            Color::Green.bold().paint("Finished")
+                    let outcome = match run_task(&outdir_path, &task) {
+                        Ok(true) => TaskOutcome::Success,
+                        Ok(false) => TaskOutcome::Failed,
+                        Err(err) => TaskOutcome::Error(err),
+                    };
+
+                    let duration = start_time.elapsed();
+                    sender.send(TaskMessage {
+                        task,
+                        outcome,
+                        duration,
+                    })
+                }
+            });
+        }
+
+        if running.is_empty() {
+            break;
         } else {
-            Color::Red.bold().paint("Failed")
-        };
-        eprintln!("{} {} (took {:.2}s)", msg, task.id, duration.as_secs_f64());
-
-        if status.success() {
-            plan.mark_done(&task.id);
-            successful.push(task);
-        } else {
-            failed.push(task);
-        };
+            match receiver.recv() {
+                Ok(result) => {
+                    running.remove(&result.task.id);
+                    match result.outcome {
+                        TaskOutcome::Success => {
+                            eprintln!(
+                                "{} {} (duration {:.2}s)",
+                                Color::Green.bold().paint("Success"),
+                                result.task.id,
+                                result.duration.as_secs_f64()
+                            );
+                            plan.mark_done(&result.task.id);
+                            successful.push(result.task);
+                        }
+                        TaskOutcome::Failed => {
+                            eprintln!(
+                                "{} {} (duration {:.2}s)",
+                                Color::Red.bold().paint("Failure"),
+                                result.task.id,
+                                result.duration.as_secs_f64()
+                            );
+                            failed.push(result.task);
+                        }
+                        TaskOutcome::Error(err) => {
+                            eprintln!(
+                                "{} {}: {} (duration {:.2}s)",
+                                Color::Red.bold().paint("  Error"),
+                                result.task.id,
+                                err,
+                                result.duration.as_secs_f64()
+                            );
+                            failed.push(result.task);
+                        }
+                    }
+                }
+                Err(err) => {
+                    eprintln!("{}: {}", Color::Red.bold().paint("Thread Failure"), err);
+                    break;
+                }
+            }
+        }
     }
 
     let not_started = plan.give_up();
@@ -120,6 +153,41 @@ fn run(opt: Opt) -> Result<()> {
     );
 
     Ok(())
+}
+
+fn run_task(outdir: &Path, task: &queue::Task<Cmd>) -> Result<bool> {
+    // Create files for redirecting output
+    let task_stdout_path = outdir.join(&task.id.as_str()).with_extension("stdout");
+    let task_stderr_path = outdir.join(&task.id.as_str()).with_extension("stderr");
+    let task_stdout = std::fs::File::create(task_stdout_path).context(Temp)?;
+    let task_stderr = std::fs::File::create(task_stderr_path).context(Temp)?;
+
+    let mut cmd = std::process::Command::new(&task.payload.program)
+        .args(&task.payload.arguments)
+        .stdout(task_stdout)
+        .stderr(task_stderr)
+        .spawn()
+        .context(TaskStart {
+            id: task.id.clone(),
+        })?;
+
+    let status = cmd.wait().context(TaskWait {
+        id: task.id.clone(),
+    })?;
+
+    Ok(status.success())
+}
+
+struct TaskMessage {
+    task: queue::Task<Cmd>,
+    outcome: TaskOutcome,
+    duration: std::time::Duration,
+}
+
+enum TaskOutcome {
+    Success,
+    Failed,
+    Error(Error),
 }
 
 #[derive(Debug)]
